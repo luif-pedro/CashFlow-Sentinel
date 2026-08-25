7d76e6fa9880a8142e4de29c8f55118ac104a0cc
Fernando <luifpedro@gmail.com>
2026-08-25 15:58:20 -0300

    feat: controla duplicidade na importacao CSV

--- data/importar_csv.py
+++ data/importar_csv.py
@@ -14,21 +14,32 @@ cursor = conn.cursor()
 
 print("CSV lido e banco conectado")
 
+importadas = 0
+duplicadas = 0
+
 for _, linha in df.iterrows():
+
     cursor.execute(
-    """
-    INSERT INTO transacoes (data, descricao, tipo, valor)
-    VALUES (%s, %s, %s, %s)
-    ON CONFLICT (data, descricao, tipo, valor) DO NOTHING
-    """,
-    (
-        linha["data"],
-        linha["descricao"],
-        linha["tipo"],
-        linha["valor"]
+        """
+        INSERT INTO transacoes (data, descricao, tipo, valor)
+        VALUES (%s, %s, %s, %s)
+        ON CONFLICT (data, descricao, tipo, valor) DO NOTHING
+        """,
+        (
+            linha["data"],
+            linha["descricao"],
+            linha["tipo"],
+            linha["valor"]
+        )
     )
-)
+
+    if cursor.rowcount == 1:
+        importadas += 1
+    else:
+        duplicadas += 1
 
 conn.commit()
 
-print("Todas as transacoes foram importadas!")
+print("Transacoes encontradas:", len(df))
+print("Transacoes importadas:", importadas)
+print("Transacoes duplicadas:", duplicadas)
