38cb9539f584200a5c8332eef4d74d82e1cf9ab3
Fernando <luifpedro@gmail.com>
2026-08-25 00:10:53 -0300

    chore: adiciona estrutura inicial do banco

---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE transacoes (
+    id SERIAL PRIMARY KEY,
+    data DATE NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    tipo VARCHAR(10) NOT NULL,
+    valor NUMERIC(12,2) NOT NULL
+);
