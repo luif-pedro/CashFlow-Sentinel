199cdc6943fe3d898bbc75ef967b3224194febec
Fernando <luifpedro@gmail.com>
2026-08-28 15:57:06 -0300

    Atualiza schema do banco

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,21 @@
+CREATE TABLE empresas (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE transacoes (
     id SERIAL PRIMARY KEY,
+    empresa_id INTEGER NOT NULL,
     data DATE NOT NULL,
     descricao VARCHAR(255) NOT NULL,
     tipo VARCHAR(10) NOT NULL,
     valor NUMERIC(12,2) NOT NULL,
-    CONSTRAINT transacao_unica UNIQUE (data, descricao, tipo, valor)
+    fingerprint VARCHAR(64),
+
+    CONSTRAINT fk_transacoes_empresa
+        FOREIGN KEY (empresa_id)
+        REFERENCES empresas(id),
+
+    CONSTRAINT transacao_unica
+        UNIQUE (empresa_id, fingerprint)
 );
